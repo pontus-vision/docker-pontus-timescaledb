46243c5e7fd5188c63ac276eb04f547c50ae0af7
dev <dev@pontusvision.com>
2019-07-01 16:29:26 +0100

    migrated files to subdirectory

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $DIR
+cd $DIR/timescaledb
 docker build --rm . -t pontusvisiongdpr/timescaledb
-
 docker push pontusvisiongdpr/timescaledb
+
+
 
 
