af8e15ad90dfc31757057ea7f4b5f94fb0c6a67b
dev <dev@pontusvision.com>
2019-07-02 15:03:30 +0100

    added postgrest image

--- build-docker.sh
+++ build-docker.sh
@@ -3,8 +3,11 @@
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR/timescaledb
 docker build --rm . -t pontusvisiongdpr/timescaledb
+
+cd $DIR/postgrest
+docker build --rm . -t pontusvisiongdpr/postgrest
+
 docker push pontusvisiongdpr/timescaledb
-
-
+docker push pontusvisiongdpr/postgrest
 
 
