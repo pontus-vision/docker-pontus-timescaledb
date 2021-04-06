cc14c80beab2e6fedf9b985e9fe6a411ce05b1eb
Leo Martins <lmartins@pontusvision.com>
2021-04-06 21:59:56 +0100

    fixed cves

--- build-docker.sh
+++ build-docker.sh
@@ -15,6 +15,10 @@ docker build   --rm . -t pontusvisiongdpr/grafana-pt
 
 cd $DIR
 
+#docker scan pontusvisiongdpr/grafana  --severity high
+#docker scan pontusvisiongdpr/grafana-pt  --severity high
+docker scan pontusvisiongdpr/timescaledb  --severity high
+docker scan pontusvisiongdpr/postgrest --severity high
 docker push pontusvisiongdpr/timescaledb
 docker push pontusvisiongdpr/postgrest
 docker push pontusvisiongdpr/grafana
