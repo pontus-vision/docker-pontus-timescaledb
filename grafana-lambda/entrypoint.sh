0f64fc0c9b54e2e2deadb555a1c218283b096af8
Leo Martins <lmartins@pontusvision.com>
2021-07-17 11:47:44 +0100

    wip

--- grafana-lambda/entrypoint.sh
+++ grafana-lambda/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-echo "GRAFANA ENTRYPOINT - 10jul2021"
+echo "GRAFANA ENTRYPOINT - 17jul2021"
 id || true
 #ls -altr /mnt || true
 #ls -altr /var/lib/grafana || true
-if [[ ! -d /mnt/grafana/pvconfig_2021-07-13T2155 ]]; then
+if [[ ! -d /mnt/grafana/pvconfig_2021-07-17T1147 ]]; then
   rm -rf /mnt/grafana/pvconfig_*
   cp  /var/lib/grafana/grafana.db /mnt/grafana/ || true
-  mkdir /mnt/grafana/pvconfig_2021-07-13T2155
+  mkdir /mnt/grafana/pvconfig_2021-07-17T1147
 fi 
 #ls -altr /run.sh || true
 echo "GRAFANA ENTRYPOINT - about to start /run.sh"
